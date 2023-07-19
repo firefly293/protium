c0eb03f14c77ae2138838eb08a22840012071444
firefly <advaitbadrish@gmail.com>
2023-07-18 18:58:06 -0700

    made guessing game example

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,31 @@ int main() {
 		CPU::op::HLT // halt
 	};
 
-	protium.StoreProgram(0x5000, factorialProgram, sizeof(factorialProgram));
+	BYTE guessingGameProgram[] = {
+		CPU::op::SETSRC, 0x1C, 0xF0, // set src to address of system random
+		CPU::op::LDA, // set a to the random value
+		CPU::op::SETB, 0x64, 0x00, // set B to 100
+		CPU::op::MOD, CPU::regID::A, CPU::regID::A, CPU::regID::B, // make random number 0 to 100
+		// start loop
+		CPU::op::ININT, CPU::regID::B, // b now has the guess
+		CPU::op::CMP, CPU::regID::A, CPU::regID::B, // compare A and B
+		CPU::op::JIZR, 0x21, 0x00, 0x00, 0x00, // jump out of loop (33 bytes forward) if equal
+		CPU::op::JISR, 0x0A, 0x00, 0x00, 0x00, // if sign, then b > a so jump 10 bytes forward
+		CPU::op::JNSR, 0x0E, 0x00, 0x00, 0x00, // if not sign, b < a so jump 14 bytes foward
+		// case b > a (JISR)
+		CPU::op::OUTIM, '>', // if b > a, output >
+		CPU::op::OUTIM, '\n', // newline
+		CPU::op::JMPR, 0x00, 0x00, 0x18, 0x00, // go back to start of loop, 24 bytes back
+		// case b < a (JNSR)
+		CPU::op::OUTIM, '<', // if b < a, output <
+		CPU::op::OUTIM, '\n', // newline
+		CPU::op::JMPR, 0x00, 0x00, 0x21, 0x00, // go back to start of loop (33 bytes back)
+		// case equal
+		CPU::op::OUTIM, '=', // if equal, output =
+		CPU::op::HLT // end
+	};
+
+	protium.StoreProgram(0x5000, guessingGameProgram, sizeof(guessingGameProgram));
 	protium.SetStartingPoint(0x5000);
 	protium.Start();
 }
